a56dbc0c8dadfeb3b8d206051a0576bf5b9fc8cc
information <01678424113b@gmail.com>
2017-11-25 10:46:30 +0700

    fix

--- app/Domain.php
+++ app/Domain.php
@@ -9,30 +9,5 @@ class Domain extends Model
     //
     protected $table = 'domains';
 
-    public function administrativeContact()
-    {
-        return $this->belongsTo(AdministrativeContact::class);
-    }
-    public function alexaInformation()
-    {
-        return $this->belongsTo(AlexaInformation::class);
-    }
-    public function domainInformation()
-    {
-        return $this->belongsTo(DomainInformation::class);
-    }
-    public function registrantContact()
-    {
-        return $this->belongsTo(RegistrantContact::class);
-    }
-    public function technicalContact()
-    {
-        return $this->belongsTo(TechnicalContact::class);
-    }
-    public function websiteInformation()
-    {
-        return $this->belongsTo(WebsiteInformation::class);
-    }
-
 
 }
